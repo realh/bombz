8309f946adfdca06b7b660f60abe29105ee01738
Tony Houghton <h@realh.co.uk>
2012-07-10 20:24:16 +0300

    Update of mscript for PNGs.

--- mscript.py
+++ mscript.py
@@ -4,12 +4,10 @@ from maitch import *
 
 SVGS_DIR = "${TOP_DIR}/svgs"
 PNGS_DIR = "${TOP_DIR}/pngs"
-TMP_PNGS_DIR = "${TOP_DIR}/tmp_pngs"
 
 ctx = Context(PACKAGE = "Bombz",
         SVGS_DIR = SVGS_DIR,
-        PNGS_DIR = PNGS_DIR,
-        TMP_PNGS_DIR = TMP_PNGS_DIR)
+        PNGS_DIR = PNGS_DIR)
 
 if ctx.mode == 'configure':
     
@@ -17,28 +15,46 @@ if ctx.mode == 'configure':
     
 elif ctx.mode == 'build':
 
-    ctx.add_rule(Rule(rule = "mkdir -p ${TGT}",
-            targets = ("${PNGS_DIR}", "${TMP_PNGS_DIR}")))
-    
     size = 72
+    # Create dest directories
     ctx.add_rule(Rule(rule = "mkdir -p ${TGT}",
-            targets = ("${PNGS_DIR}/%d" % size,
-                    "${TMP_PNGS_DIR}/%d" % size),
-            deps = "${PNGS_DIR}"))
-    for name in "bomb1 bomb2 manup manleft mandown manright " \
+            targets = ("${PNGS_DIR}/%d/fg" % size,
+            "${PNGS_DIR}/%d/tiles" % size)))
+    # Dirt PNG from XCF
+    ctx.add_rule(Rule(rule = "${CONVERT} ${SRC} ${TGT}",
+            targets = "${PNGS_DIR}/%d/tiles/texture.png" % size,
+            sources = "${TOP_DIR}/texture/dirt.xcf",
+            deps = "${PNGS_DIR}/%d/tiles" % size,
+            where = TOP))
+    # Floor (not transparent)
+    ctx.add_rule(Rule(rule = "${CONVERT} ${SRC} -geometry %dx%d ${TGT}" %
+                    (size, size),
+            targets = "${PNGS_DIR}/%d/tiles/floor.png" % size,
+            sources = "${SVGS_DIR}/floor.svg",
+            deps = "${PNGS_DIR}/%d/tiles" % size,
+            where = TOP))
+    # Foregrounds to be overlaid over floor texture, and droid character
+    for name in "bomb1 bomb2 droidup droidleft droiddown droidright " \
             "match picket".split() + \
             ["explo%02d" % n for n in range(1, 12)]:
         ctx.add_rule(Rule(rule = "${CONVERT} -background #0000 " \
                 "${SRC} -geometry %dx%d ${TGT}" % (size, size),
-                targets = "${PNGS_DIR}/%d/%s.png" % (size, name),
+                targets = "${PNGS_DIR}/%d/fg/%s.png" % (size, name),
                 sources = "${SVGS_DIR}/%s.svg" % name,
-                deps = "${PNGS_DIR}/%d" % size,
+                deps = "${PNGS_DIR}/%d/fg" % size,
                 where = TOP))
+    # Big flash for start of explosion
+    ctx.add_rule(Rule(rule = "${CONVERT} -background #0000 " \
+            "${SRC} -geometry %dx%d ${TGT}" % (size * 3, size * 3),
+            targets = "${PNGS_DIR}/%d/fg/explo00.png" % size,
+            sources = "${SVGS_DIR}/explo00.svg",
+            deps = "${PNGS_DIR}/%d/fg" % size,
+            where = TOP))
 
 elif ctx.mode == 'clean' or ctx.mode == 'pristine':
 
     ctx.recursively_remove("${PNGS_DIR}")
-    ctx.recursively_remove("${TMP_PNGS_DIR}")
+    ctx.delete(ctx.glob("*.png", "${TOP_DIR}/texture"))
     if ctx.mode == 'pristine':
         ctx.delete("${TOP_DIR}/maitch.pyc")
         ctx.recursively_remove("${TOP_DIR}/build")
