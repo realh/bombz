8a67c9547707668dc06737d0279fdb5f3ca0be4b
Tony Houghton <h@realh.co.uk>
2012-07-16 00:49:19 +0100

    Added Log source files.

---
+++ hgame/Log.cpp
@@ -0,0 +1,192 @@
+/*
+ * Copyright (c) 2012, Tony Houghton <h@realh.co.uk>
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met: 
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer. 
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution. 
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+// HGame - a simple cross-platform game framework
+
+// Log.cpp: Simple logging implementation
+
+#include "Log.h"
+
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+
+namespace hgame {
+
+// For some reason these aren't in std::; use wrappers in case they are one day
+std::FILE *cstderr = stderr;
+
+inline char *cstrdup(const char *s) { return strdup(s); }
+
+inline int cvasprintf(char **sout, const char *fmt, std::va_list ap)
+{
+    return vasprintf(sout, fmt, ap);
+}
+
+static int casprintf(char **sout, const char *fmt, ...)
+{
+    std::va_list ap;
+    va_start(ap, fmt);
+    int result = vasprintf(sout, fmt, ap);
+    va_end(ap);
+    return result;
+}
+
+ 
+void Log::logWrite(Level level, const Location *loc, const char *message)
+{
+    std::fprintf(cstderr, "%-7s '%s' at %s: %s\n",
+            getLevelName(level), tag, loc->getString(), message);
+    if (level == FATAL)
+        abort();
+}
+    
+const char *Log::getLevelName(Level level) const
+{
+    switch (level)
+    {
+        case FATAL:
+            return "FATAL";
+        case ERROR:
+            return "ERROR";
+        case WARNING:
+            return "WARNING";
+        case INFO:
+            return "INFO";
+        case DEBUG:
+            return "DEBUG";
+        case VERBOSE:
+            return "VERBOSE";
+    }
+    return NULL;
+}
+
+Log::Log(const char *tag, Level priority)
+{
+    this->tag = cstrdup(tag);
+    this->priority = priority;
+}
+
+Log::~Log()
+{
+    // Not delete because we used strdup()
+    std::free(tag);
+}
+    
+void Log::log(Level level, Location *loc, const char *format, ...)
+{
+    if (level <= priority)
+    {
+        std::va_list ap;
+        va_start(ap, format);
+        log(level, loc, format, ap);
+        va_end(ap);
+    }
+    delete loc;
+}
+    
+void Log::log(Level level, Location *loc, const char *format, std::va_list ap)
+{
+    if (level <= priority)
+    {
+        char *s;
+        cvasprintf(&s, format, ap);
+        logWrite(level, loc, s);
+        std::free(s);
+    }
+    else
+    {
+        delete loc;
+    }
+}
+
+#define IMPLEMENT_LOG_LEVEL(l) \
+    std::va_list ap; \
+    va_start(ap, format); \
+    log(l, loc, format, ap); \
+    va_end(ap);
+
+void Log::f(Location *loc, const char * format, ...)
+{
+    IMPLEMENT_LOG_LEVEL(FATAL);
+}
+
+void Log::e(Location *loc, const char * format, ...)
+{
+    IMPLEMENT_LOG_LEVEL(ERROR);
+}
+
+void Log::w(Location *loc, const char * format, ...)
+{
+    IMPLEMENT_LOG_LEVEL(WARNING);
+}
+
+void Log::i(Location *loc, const char * format, ...)
+{
+    IMPLEMENT_LOG_LEVEL(INFO);
+}
+
+void Log::d(Location *loc, const char * format, ...)
+{
+    IMPLEMENT_LOG_LEVEL(DEBUG);
+}
+
+void Log::v(Location *loc, const char * format, ...)
+{
+    IMPLEMENT_LOG_LEVEL(VERBOSE);
+}
+        
+Log::LocationStr::LocationStr(const char *s)
+{
+    s = cstrdup(s);
+}
+
+Log::LocationStr::LocationStr(const char *srcfile, int line, const char *func)
+{
+    if (func)
+        casprintf(&s, "line %d of file %s (function %s)", line, srcfile, func);
+    else
+        casprintf(&s, "line %d of file %s", line, srcfile);
+}
+        
+Log::LocationStr::~LocationStr()
+{
+    std::free(s);
+}
+        
+void Log::LocationStr::setString(const char *s)
+{
+    std::free(this->s);
+    this->s = cstrdup(s);
+}
+
+const char *Log::LocationStr::getString() const
+{
+    return s;
+}
+
+}
---
+++ hgame/Log.h
@@ -0,0 +1,124 @@
+/*
+ * Copyright (c) 2012, Tony Houghton <h@realh.co.uk>
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met: 
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer. 
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution. 
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+// HGame - a simple cross-platform game framework
+
+// Log.h: Interface for logging based on Android
+
+#ifndef LOG_H
+#define LOG_H
+
+#include <cstdarg>
+
+namespace hgame {
+ 
+class Log
+{
+public:
+    enum Level {
+        FATAL,      // Causes abort
+        ERROR,
+        WARNING,
+        INFO,
+        DEBUG,
+        VERBOSE
+    };
+    
+    const char *getLevelName(Level level) const;
+    
+    // Only messages with given priority or higher will be logged
+    Log(const char *tag, Level priority = DEBUG);
+    
+    ~Log();
+    
+    // Abstract class to encapsulate information about where an error occured.
+    class Location
+    {
+    public:
+        virtual ~Location() = 0;
+        virtual const char *getString() const = 0;
+        inline operator const char *() const
+        {
+            return getString();
+        }
+    };
+    
+    // Default implementation writes info to stderr.
+    // Derived classes can re-implement this
+    virtual void logWrite(Level level, const Location *loc,
+            const char *message);
+
+public:
+    // Formats a message before passing it on to logWrite.
+    // loc is not const because log() deletes it
+    virtual void log(Level level, Location *loc, const char *format, ...);
+    virtual void log(Level level, Location *loc,
+            const char *format, std::va_list ap);
+    
+    void f(Location *loc, const char *format, ...);
+    void e(Location *loc, const char *format, ...);
+    void w(Location *loc, const char *format, ...);
+    void i(Location *loc, const char *format, ...);
+    void d(Location *loc, const char *format, ...);
+    void v(Location *loc, const char *format, ...);
+    
+    // Simple Location where only info is a string
+    class LocationStr : public Location
+    {
+    protected:
+        char *s;
+    public:
+        LocationStr(const char *s);
+        // Most likely called with args (__FILE__, __LINE__
+        //                               [, __PRETTY_FUNCTION__])
+        LocationStr(const char *srcfile, int line, const char *func = 0);
+        ~LocationStr();
+        void setString(const char *s);
+        const char *getString() const;
+    };
+
+private:
+    char *tag;
+    Level priority;
+
+};
+
+}
+
+// Convenience macros
+#define HGAME_LOG(lgr, lvl, fmt, ...) (lgr)->log((lvl), \
+        new hgame::Log.LocationStr(__FILE__, __LINE__, __PRETTY_FUNCTION__), \
+        fmt, ...)
+
+#define HGAME_F(lgr, fmt, ...) HGAME_LOG((lgr), hgame::Log::FATAL, (fmt), ...)
+#define HGAME_E(lgr, fmt, ...) HGAME_LOG((lgr), hgame::Log::ERROR, (fmt), ...)
+#define HGAME_W(lgr, fmt, ...) HGAME_LOG((lgr), hgame::Log::WARNING, (fmt), ...)
+#define HGAME_I(lgr, fmt, ...) HGAME_LOG((lgr), hgame::Log::INFO, (fmt), ...)
+#define HGAME_D(lgr, fmt, ...) HGAME_LOG((lgr), hgame::Log::DEBUG, (fmt), ...)
+#define HGAME_V(lgr, fmt, ...) HGAME_LOG((lgr), hgame::Log::VERBOSE, (fmt), ...)
+
+#endif // LOG_H
