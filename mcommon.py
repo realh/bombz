96682ae50ea1a2a2c74053997bba99f362e1f04c
Tony Houghton <h@realh.co.uk>
2012-07-16 00:46:32 +0100

    git seemed to miss these modifications last time.

--- mcommon.py
+++ mcommon.py
@@ -1,5 +1,6 @@
 envs = {
-    LOCAL_DATA_DIR: "${TOP_DIR}/share/bombz",
-    SRC_DIR: "${TOP_DIR}",
+    "PACKAGE": "bombz",
+    "LOCAL_DATA_DIR": "${TOP_DIR}/share/bombz",
+    "SRC_DIR": "${TOP_DIR}",
 }
 
