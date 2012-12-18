f13730cd308dfab2b3e09af657bb4557f97966b0
Tony Houghton <h@realh.co.uk>
2012-12-18 19:33:07 +0000

    Added Eclipse C/C++ support.

--- jni/Android.mk
+++ jni/Android.mk
@@ -4,7 +4,8 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := bombz
 LOCAL_SRC_FILES := \
 		$(wildcard hgame/*.cpp) \
-		$(wildcard handroid/*.cpp)
+		$(wildcard handroid/*.cpp) \
+		$(wildcard bombz/*.cpp)
 
 LOCAL_LDLIBS := -landroid -llog
 LOCAL_STATIC_LIBRARIES := android_native_app_glue
