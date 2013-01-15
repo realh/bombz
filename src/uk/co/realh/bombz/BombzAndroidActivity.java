1a5c17f9a2289c473806e2ee0e76f7c4ae31d170
Tony Houghton <h@realh.co.uk>
2013-01-15 20:44:34 +0000

    Added test for Sys.listFolder.

--- src/uk/co/realh/bombz/BombzAndroidActivity.java
+++ src/uk/co/realh/bombz/BombzAndroidActivity.java
@@ -5,18 +5,41 @@ import android.app.Activity;
 //import android.view.Menu;
 
 import uk.co.realh.hgame.Log;
+import uk.co.realh.hgame.Sys;
 import uk.co.realh.hgame.android.AndroidLog;
+import uk.co.realh.hgame.android.AndroidSys;
 
 /**
  * @author Tony Houghton
  */
 public class BombzAndroidActivity extends Activity {
+	
+	private static final String TAG = "Activity";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		Log.setLogger(new AndroidLog());
 		setContentView(R.layout.activity_bombz_android);
+		Sys sys;
+		String[] subdirs;
+		try {
+			sys = new AndroidSys(this,
+					"realh", "bombz.sf.net", "Bombz", "uk.co.realh.bombz");
+			subdirs = sys.listFolder("pngs");
+		} catch (Throwable e) {
+			Log.e(TAG, "Can't list pngs folder(s)", e);
+			return;
+		}
+		String subs = null;
+		for (String sub: subdirs)
+		{
+			if (subs == null)
+				subs = sub;
+			else
+				subs += ", " + sub;
+		}
+		Log.i(TAG, "subs contains: " + subs);
 	}
 
 	/*
